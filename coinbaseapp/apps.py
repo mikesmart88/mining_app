d9482823fcf0b1521c75a4abce6ec48c3223f6ba
uzomamicheal <uzomamicheal062@gmail.com>
2024-11-08 14:07:59 +0100

    stating project and setup

---
+++ coinbaseapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CoinbaseappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'coinbaseapp'
---
+++ coinbaseapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path as ph
+
+# create your urls for your app here 
+
+app_name = 'coinbaseapp'
+
+urlpatterns = [
+    
+]
+
